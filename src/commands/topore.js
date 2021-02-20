4a906aff01c3bc059578bcf8265632187b964f2a
Econmartin <73027064+Econmartin@users.noreply.github.com>
2021-02-20 21:25:34 +0000

    reroute file?

--- src/commands/topore.js
+++ src/commands/topore.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
-const report = require('../report.js');
+const report = require('../report');
 const oreChannelID = '757717773010075649';
 
 function formatMoney(number) {
