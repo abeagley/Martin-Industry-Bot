c42ae0d1c7b61c052dc5dd92f9993c771b8dea5b
Econmartin <73027064+Econmartin@users.noreply.github.com>
2021-02-20 21:28:55 +0000

    js add

--- src/commands/topore.js
+++ src/commands/topore.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
 const mongoose = require('mongoose');
 const report = require('../report');
 const oreChannelID = '757717773010075649';
