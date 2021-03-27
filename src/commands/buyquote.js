d61f63b11a14a806fb3c2a46b88c54edd3584bdf
Martin-1 <martinsoftwaredev@outlook.com>
2021-03-27 14:35:59 +0000

    adjusted require buyPrices

--- src/commands/buyquote.js
+++ src/commands/buyquote.js
@@ -1,4 +1,4 @@
-prices = require('../prices/pilotBuyPrices')
+require('../prices/pilotBuyPrices');
 
 let quoteTotal = [];
 
@@ -10,9 +10,9 @@ module.exports = async (message, args) => {
 	if (args.length < 2) {return message.reply('No Values Input :pensive: Try \'!quote veldspar 1000 scordite 1000...\'');}
 	else {
 		for (let i = 0; i < args.length; i++) {
-			for (let j = 0; j < prices.length; j++) {
-				if (args[i] === prices[j][0]) {
-					quoteTotal.push(args[i+1] * prices[j][1]);
+			for (let j = 0; j < buyPrices.length; j++) {
+				if (args[i] === buyPrices[j][0]) {
+					quoteTotal.push(args[i+1] * buyPrices[j][1]);
 				}
 				console.log(quoteTotal);
 			}  
