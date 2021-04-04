689df15255e6afde0caaee5eb987d9c860c8d273
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-04 22:09:10 +0100

    Added some notes to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
+// Welcome to EE Industry Bot
+
+// index.js runs through heroku
+// commands/ holds all commands used by the bot
+// command handler is in /commands/index.js
+// database/ holds database requests / transformations
+// models holds mongoDB schemas
+// prices holds current corp buy ands sell prices
+
+// discord for message handling
 const Discord = require('discord.js');
+// mongoose for database handling
 const mongoose = require('mongoose');
+
+// token and mongoDB_URI passwords are hosted on heroku
+// see Procfile for heroku worker startup
 const { prefix, token, MONGODB_URI } = require('./config.json');
+
+// connect to discord
 const client = new Discord.Client();
+// connect to command handler
 const commandHandler = require('./commands');
-
-
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -15,10 +27,13 @@ client.once('ready', () => {
 
 client.on('message', commandHandler);
 
+// login to discord bot
 client.login(process.env.token);
+// connect to database
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 	console.log('we\'re connected!');
 });
+
