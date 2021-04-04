6d842f0b1c455b7e0b632bf423a589e16819a912
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-05 00:09:41 +0100

    delete changes

--- src/commands/sellprices.js
+++ src/commands/sellprices.js
@@ -8,28 +8,8 @@ module.exports = async (message) =>  {
 
         let sellPrices = require('../prices/pilotSellPrices')
         let stringP = sellPrices.toString()
-        let stringOutput = "";
-        for (let i = 0; i < stringP.length; i++) {
-            stringOutput += `${stringP[i]} \n`
-        }
         console.log(stringP)
-        console.log(stringOutput)
-
-        let sellPriceMessage = new Discord.MessageEmbed()
-            .setTitle('Current Sell to Corp Prices')
-            .setColor(15105570)
-            .addFields({ name: 'Prices', value: stringOutput, inline: true}
-            )
-            .setTimestamp()
-            .setFooter('Thanks for doing business with HTP')
-        ;
-        await message.channel.send(sellPriceMessage);
-
-
-
-
-
-
+        await message.reply(stringP);
     }
     else {
         await message.reply("Incorrect ID or Channel");
