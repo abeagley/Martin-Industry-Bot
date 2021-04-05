ac94dc8ae6350c29fe6175cb204a1dca95f0b229
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-05 20:36:40 +0100

    try timeout on runOnce in buyquote.js

--- src/commands/buyquote.js
+++ src/commands/buyquote.js
@@ -2,34 +2,29 @@
 
 let mongo = require('../mongo')
 let quoteTotal = [];
-let quoteOutput = 0;
 
 function formatMoney(number) {
 	return number.toLocaleString('en-US', { style: 'decimal', currency: 'USD' });
 }
 
 module.exports = async (message, args) => {
+	await mongo();
 	let buyPrices = require('../prices/pilotBuyPrices');
-	function runOnce(res) {
-		function runThis() {
-			if (args.length < 2) {
-				return message.reply('No Values Input :pensive: Try \'!quote veldspar 1000 scordite 1000...\'');
-			} else {
-				for (let i = 0; i < args.length; i++) {
-					for (let j = 0; j < buyPrices.length; j++) {
-						if (args[i] === buyPrices[j][0]) {
-							quoteTotal.push(args[i + 1] * buyPrices[j][1]);
-						}
-						console.log(quoteTotal);
-					}
+	if (args.length < 2) {return message.reply('No Values Input :pensive: Try \'!quote veldspar 1000 scordite 1000...\'');}
+	else {
+		for (let i = 0; i < args.length; i++) {
+			for (let j = 0; j < buyPrices.length; j++) {
+				if (args[i] === buyPrices[j][0]) {
+					quoteTotal.push(args[i+1] * buyPrices[j][1]);
 				}
-				quoteOutput = quoteTotal.reduce((a, b) => a + b, 0);
-
+				console.log(quoteTotal);
 			}
 		}
-		runThis()
-		message.reply(formatMoney(res));
+		const quoteOutput = quoteTotal.reduce((a,b) => a+b,0);
+
+		function runOnce(){message.reply(formatMoney(quoteOutput));}
+		setTimeout(runOnce,2000)
+
 	}
-	runOnce()
 	quoteTotal = [];
 };
