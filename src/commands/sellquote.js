3f26715035d88bcba5dc8ae8007af6164f24d29a
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-11 15:42:04 +0100

    log check

--- src/commands/sellquote.js
+++ src/commands/sellquote.js
@@ -12,7 +12,7 @@ function formatMoney(number) {
 module.exports = async (message, args) => {
 
 	function loop() {
-		return new Promise(async resolve  =>  {
+		return new Promise(async (resolve, reject)  =>  {
 			const sellPrices = await require('../prices/pilotSellPrices')
 
 			for (let i = 0; i < args.length; i++) {
@@ -25,15 +25,24 @@ module.exports = async (message, args) => {
 					//console.log(argTotal);
 				}
 			}
+			console.log("Quotetotal length" + quoteTotal.length)
+			console.log("Arg length over 2" + (args.length / 2))
 			if (quoteTotal.length !== 0 && quoteTotal.length === (args.length / 2)) {
 				console.log("Done 1")
 				resolve();
 			}
+			else {
+				console.log("rejected")
+				reject();
+			}
+
 		})
 	}
 
 	function messageSend() {
 		let quoteOutput = quoteTotal.reduce((a, b) => a + b, 0);
+		console.log(quoteOutput)
+		console.log("3")
 		sellquote = new Discord.MessageEmbed()
 				.setTitle('Quote')
 				.setAuthor(message.member.nickname, message.author.avatarURL())
@@ -45,12 +54,14 @@ module.exports = async (message, args) => {
 				.setTimestamp()
 				.setFooter('Oh look it worked')
 			;
+		console.log("4")
 		message.channel.send(sellquote)
 	}
 
 	async function callback1(result) {
 		try {
 			await loop(result)
+			console.log("2")
 			console.log(result)
 		}
 		catch (error) {
