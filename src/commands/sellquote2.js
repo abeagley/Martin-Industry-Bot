cd4a60df46881c9d0b8858ea8f4d501e73357d5d
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-11 19:00:02 +0100

    creates async loop

--- src/commands/sellquote2.js
+++ src/commands/sellquote2.js
@@ -9,32 +9,47 @@ function formatMoney(number) {
 	return number.toLocaleString('en-US', { style: 'decimal', currency: 'USD' });
 }
 
-module.exports = (message, args) => {
+module.exports = async (message, args) => {
 	const sellPrices = require('../prices/pilotSellPrices')
 
-	for (let i = 0; i < args.length; i++) {
-		for (let j = 0; j < sellPrices.length; j++) {
-			if (args[i].toLowerCase() === sellPrices[j][0]) {
-				//argTotal.push(args[i],args[i]* sellPrices[j][1]);
-				quoteTotal.push(args[i + 1] * sellPrices[j][1]);
-			}
+	const waitFor = (ms) => new Promise( r=> setTimeout(r, ms));
+
+	async function
+	asyncForEach(array, callback) {
+		for (let j = 0; j < array.length; j++) {
+			await callback(Array[j], j, array);
 		}
 	}
 
-	let quoteOutput = quoteTotal.reduce((a, b) => a + b, 0);
+	const start = async () => {
+		for (let i = 0; i < args.length; i++) {
+		await asyncForEach(sellPrices, async (num) => {
+			await waitFor(50)
+				if (args[i].toLowerCase() === num[j][0]) {
+					//argTotal.push(args[i],args[i]* sellPrices[j][1]);
+					quoteTotal.push(args[i + 1] * num[j][1]);
+				}
+			})
+		}
+	}
 
-	sellquote = new Discord.MessageEmbed()
-		.setTitle('Quote')
-		.setAuthor(message.member.nickname, message.author.avatarURL())
-		.setColor(15105570)
-		.addFields(
-			//{name: 'Items:', value: argTotal},
-			{name: 'Total isk', value: formatMoney(quoteOutput)}
-		)
-		.setTimestamp()
-		.setFooter('Oh look it worked')
-	;
+	const two = () => {
+		let quoteOutput = quoteTotal.reduce((a, b) => a + b, 0);
 
-	message.channel.send(sellquote)
+		sellquote = new Discord.MessageEmbed()
+			.setTitle('Quote')
+			.setAuthor(message.member.nickname, message.author.avatarURL())
+			.setColor(15105570)
+			.addFields(
+				//{name: 'Items:', value: argTotal},
+				{name: 'Total isk', value: formatMoney(quoteOutput)}
+			)
+			.setTimestamp()
+			.setFooter('Oh look it worked')
+		;
+		message.channel.send(sellquote)
+	}
+	await start().then(await two())
+
 }
 quoteTotal = [];
