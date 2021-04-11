370728576543cb6cb6ee98acd19dd45b49a0c614
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-11 01:06:12 +0100

    restructure into functions + await

--- src/commands/sellquote.js
+++ src/commands/sellquote.js
@@ -9,71 +9,52 @@ function formatMoney(number) {
 	return number.toLocaleString('en-US', { style: 'decimal', currency: 'USD' });
 }
 
-const getP = async () => {
-	let sellPrices = require('../prices/pilotSellPrices');
-	await console.log(sellPrices);
-	return sellPrices
-}
 
 module.exports = async (message, args) => {
-	await getP()
-		.then( async (sellPrices,err) => {
-			if (err) {
-				console.log(err)
-			}
-			else {
-				console.log("Starting Calc")
-				let loop = async function () {
-					for (let i = 0; i < args.length; i++) {
-						for (let j = 0; j < sellPrices.length; j++) {
-							if (args[i].toLowerCase() === sellPrices[j][0]) {
-								//argTotal.push(args[i],args[i]* sellPrices[j][1]);
-								await quoteTotal.push(args[i + 1] * sellPrices[j][1]);
 
-							}
-							console.log(quoteTotal);
-							//console.log(argTotal);
-						}
+	async function getPrices() {
+		let sellPrices = require('../prices/pilotSellPrices');
+		await console.log(sellPrices);
+		return sellPrices
+	}
+
+	async function loop(sellPrices) {
+		console.log("Starting Calc")
+		for (let i = 0; i < args.length; i++) {
+			for (let j = 0; j < sellPrices.length; j++) {
+				if (args[i].toLowerCase() === sellPrices[j][0]) {
+					//argTotal.push(args[i],args[i]* sellPrices[j][1]);
+					await quoteTotal.push(args[i + 1] * sellPrices[j][1]);
 					}
-					return quoteTotal
+					console.log(quoteTotal);
+					//console.log(argTotal);
 				}
-				await loop()
-					.then(async (quoteTotal,err) => {
-						if (err) {
-							console.log(err)
-						}
-						else {
-							let message1 = async function () {
-							const quoteOutput = await quoteTotal.reduce((a, b) => a + b, 0);
-
-							sellquote = new Discord.MessageEmbed()
-								.setTitle('Quote')
-								.setAuthor(message.member.nickname, message.author.avatarURL())
-								.setColor(15105570)
-								.addFields(
-									//{name: 'Items:', value: argTotal},
-									{name: 'Total isk', value: formatMoney(quoteOutput)}
-								)
-								.setTimestamp()
-								.setFooter('Oh look it worked')
-							;
-							return sellquote
-						}
-							await message1()
-								.then((sellquote,err) => {
-									if (err) {
-										console.log(err)
-									}
-									else {
-										console.log(sellquote)
-										message.channel.send(sellquote)
-									}
-								})}
-
-					})
 			}
+			return quoteTotal
+	}
 
-		})
+	async function message1(quoteTotal) {
+		const quoteOutput = await quoteTotal.reduce((a, b) => a + b, 0);
+
+		sellquote = new Discord.MessageEmbed()
+			.setTitle('Quote')
+			.setAuthor(message.member.nickname, message.author.avatarURL())
+			.setColor(15105570)
+			.addFields(
+				//{name: 'Items:', value: argTotal},
+				{name: 'Total isk', value: formatMoney(quoteOutput)}
+				)
+			.setTimestamp()
+			.setFooter('Oh look it worked')
+		;
+		return sellquote
+	}
+
+	await getPrices()
+		.then(await loop(sellPrices)
+			.then(await message1(quoteTotal)
+				.then (message.channel.send(sellquote))))
+
 }
 
 quoteTotal = [];
