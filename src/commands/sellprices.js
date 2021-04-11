1f887bf461cfec182c64b14f55ec88cde12130bf
Martin-1 <martinsoftwaredev@outlook.com>
2021-04-12 00:32:40 +0100

    rework to sellPrices

--- src/commands/sellprices.js
+++ src/commands/sellprices.js
@@ -2,14 +2,31 @@ const Discord = require('discord.js');
 const orderChannelID = '756565959073857679';
 const econID = '722877594709524501'
 const mongoose = require('mongoose');
+const mongo = require('../mongo');
+const Report = require('../models/pilotSellPrices');
 
 module.exports = async (message) =>  {
     if (message.channel.id === orderChannelID && message.author.id === econID ) {
+        await mongo().then(async function () {
+            function getSPrices(callback) {
+                Report.findOne().sort({createdAt: -1}).limit(1).exec((err, getPrice) => {
+                    if (err) callback(err, null);
+                    else callback(null, getPrice);
+                })
+            }
 
-        let sellPrices = require('../prices/pilotSellPrices')
-        let stringP = sellPrices.toString()
-        console.log(stringP)
-        await message.reply(stringP);
+            let sPrices = [];
+            await getSPrices(async function (err, priceResult) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(priceResult);
+                    sPrices = priceResult.prices;
+                    console.log(sPrices);
+                    message.reply("`​`​`" + sPrices + "`​`​`");
+                }
+            })
+        })
     }
     else {
         await message.reply("Incorrect ID or Channel");
