315669309b8786a96225959aca5d985055dfc4ef
Martin-1 <martinsoftwaredev@outlook.com>
2021-08-03 23:16:25 +0100

    add ffmpeg

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -4,9 +4,10 @@ module.exports = (message, args, client) => {
     const VCchannelID = ('770358282752884776');
     const VCchannel = message.guild.channels.cache.get(VCchannelID);
     VCchannel.join();
-    message.channel.send(`<@&770332037763825664> ${args}`, {
+    message.channel.send(` ${args}`, {
         tts: true
     });
     const broadcast = message.client.voice.createBroadcast();
     broadcast.play('./src/assets/92a61070-f4a5-11eb-bdb4-d3cdde3ec4b5.mp3', { volume: 1 });
 }
+//<@&770332037763825664>
