3c446a946599269a7e23d528591bf21137874798
Martin-1 <martinsoftwaredev@outlook.com>
2021-08-03 22:30:52 +0100

    add tts with tag

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
-module.exports = (message) => {
+module.exports = (message, args) => {
     console.log("doing alert again")
     const VCchannelID = ('770358282752884776');
     const VCchannel = message.guild.channels.cache.get(VCchannelID);
     VCchannel.join();
+    message.channel.send(`<@&${770332037763825664}> ${args}`, {
+        tts: true
+    });
 }
