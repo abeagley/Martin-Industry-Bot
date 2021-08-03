58ae952e433f25daccade5d4089b425e5d1a73eb
Martin-1 <martinsoftwaredev@outlook.com>
2021-08-03 22:59:00 +0100

    add voice sound

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-module.exports = (message, args) => {
+module.exports = (message, args, client) => {
     console.log("doing alert again")
     const VCchannelID = ('770358282752884776');
     const VCchannel = message.guild.channels.cache.get(VCchannelID);
@@ -7,4 +7,6 @@ module.exports = (message, args) => {
     message.channel.send(`<@&770332037763825664> ${args}`, {
         tts: true
     });
+    const broadcast = message.voice.createBroadcast();
+    broadcast.play('./src/assets/92a61070-f4a5-11eb-bdb4-d3cdde3ec4b5.mp3', { volume: 1 });
 }
