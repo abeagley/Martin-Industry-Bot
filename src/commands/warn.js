4ace3ca0143586b68694a77de6032dfef9142651
Martin-1 <martinsoftwaredev@outlook.com>
2021-08-03 22:09:23 +0100

    rename to warnadd discord.js to warn

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -1,7 +1,7 @@
-
+const Discord = require('discord.js');
 module.exports = () => {
     console.log("doing alert")
     const VCchannelID = ('770358282752884776');
-    const VCchannel = message.guild.channels.cache.get(VCchannelID);
+    const VCchannel = Discord.guild.channels.cache.get(VCchannelID);
     VCchannel.join();
 }
