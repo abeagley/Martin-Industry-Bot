c8d4c4aaf7ed0b70f0fe11e042b02ec9653cacfa
Martin-1 <martinsoftwaredev@outlook.com>
2021-12-06 12:34:55 +0000

    revert schema to find one to check eroor of mongo end

--- src/models/priceListSchema.js
+++ src/models/priceListSchema.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
+const reqString = {
+    type: String,
+    required: true
+}
+
 const priceListSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    sell_price: {type: Number},
+    buy_price: {type: Number}
 },{
     timestamps: true
 });
