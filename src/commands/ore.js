710646db5d6823314ede62858587ff6447edf718
Martin-1 <martinsoftwaredev@outlook.com>
2021-12-18 11:51:47 +0000

    Add pi changes to ore

--- src/commands/ore.js
+++ src/commands/ore.js
@@ -10,6 +10,7 @@ const sellReport = require('../models/priceListSchema');
 let quoteTotalTwo = [];
 let oreArray = [];
 let oreNumber = [];
+let reportArray = [];
 
 function formatMoney(number) {
 	return number.toLocaleString('en-US', { style: 'decimal', currency: 'USD' });
@@ -19,7 +20,9 @@ function formatMoney(number) {
 module.exports = async (message, args) =>  {
 	if (args.length < 2) {
 		return message.reply('No Values Input :pensive: Try \'!ore veldspar 1000 scordite 1000...\'');
-	} else {
+	}
+	else if (args.length > 21) {return message.reply('Too many values :pensive: Keep it to 10 or less ')}
+	else {
 		await mongo().then(async function () {
 			function getSPrices(callback) {
 				sellReport.find({}).exec((err, getPrice) => {
@@ -41,9 +44,17 @@ module.exports = async (message, args) =>  {
 					for (let i = 0; i < args.length; i++) {
 						for (let j = 0; j < sPrices.length; j++) {
 							if (args[i].toLowerCase() === sPrices[j].item) {
+								reportArray.push(args[i])
+								console.log(reportArray)
 								let sellPrice = parseFloat(sPrices[j].sell_price)
 								console.log(sPrices[j].sell_price)
+								reportArray.push(sellPrice)
+								console.log(reportArray)
+								reportArray.push(args[i+1])
+								console.log(reportArray)
 								quoteTotalTwo.push(args[i + 1] * sellPrice);
+								reportArray.push(args[i + 1] * sellPi)
+								console.log(reportArray);
 							}
 							console.log(quoteTotalTwo);
 						}
@@ -62,6 +73,17 @@ module.exports = async (message, args) =>  {
 					let quoteOutputTwo = quoteTotalTwo.reduce((a, b) => a + b, 0);
 					quoteOutputTwo = Math.round(quoteOutputTwo)
 
+					let errorVal = (args.length / 2);
+					let reportVal = (reportArray.length / 4)
+					let errorName = "";
+
+					if (errorVal === reportVal){
+						errorName = "No"
+					}
+					else {
+						errorName = "Yes"
+					}
+
 					const disagree1 = '❌';
 					const agree1 = '☑️';
 
@@ -70,8 +92,17 @@ module.exports = async (message, args) =>  {
 						.setTitle('Status: Sell Ore Request')
 						.setAuthor(message.member.nickname, message.author.avatarURL())
 						.setColor(15105570)
-						.addFields({name: args, value: '----------', inline: true},
-							{name: 'Total isk', value: formatMoney(quoteOutputTwo)}
+						.addFields({name: reportArray[2] + " " + reportArray[0] + " @ " + reportArray[1] , value: reportArray[3], inline: true},
+							{name: reportArray[6] + " " + reportArray[4] + " @ " + reportArray[5] , value: reportArray[7], inline: true},
+							{name: reportArray[10] + " " + reportArray[8] + " @ " + reportArray[9] , value: reportArray[11], inline: true},
+							{name: reportArray[14] + " " + reportArray[12] + " @ " + reportArray[13] , value: reportArray[15], inline: true},
+							{name: reportArray[18] + " " + reportArray[16] + " @ " + reportArray[17] , value: reportArray[19], inline: true},
+							{name: reportArray[22] + " " + reportArray[20] + " @ " + reportArray[21] , value: reportArray[23], inline: true},
+							{name: reportArray[26] + " " + reportArray[24] + " @ " + reportArray[25] , value: reportArray[27], inline: true},
+							{name: reportArray[30] + " " + reportArray[28] + " @ " + reportArray[29] , value: reportArray[31], inline: true},
+							{name: reportArray[34] + " " + reportArray[32] + " @ " + reportArray[33] , value: reportArray[35], inline: true},
+							{name: reportArray[38] + " " + reportArray[36] + " @ " + reportArray[37] , value: reportArray[39], inline: true},
+							{name: 'Total isk', value: formatMoney(piOutput)}, {name: "Errors?", value: errorName }
 						)
 						.setTimestamp()
 						.setFooter('Please ping a director asking where contract should go to request')
@@ -147,4 +178,5 @@ module.exports = async (message, args) =>  {
 	}
 
 	quoteTotalTwo = [];
+	reportArray = [];
 }
