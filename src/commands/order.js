77d51c24062ee240cf058a022b074ec4b62f6f67
Martin-1 <martinsoftwaredev@outlook.com>
2021-12-18 12:13:51 +0000

    updates to order reflecting new rules

--- src/commands/order.js
+++ src/commands/order.js
@@ -11,6 +11,7 @@ const Report = require('../models/priceListSchema');
 let quoteTotalThree = [];
 let orderArray = [];
 let orderNumber = [];
+let reportArray = [];
 
 // Function to have isk value with commas 
 function formatMoney(number) {
@@ -22,6 +23,7 @@ function formatMoney(number) {
 module.exports = async (message, args) => {
 	// Check bot is in the right channel
 		if (args.length < 2) {return message.reply('No Values Input :pensive: Try \'!order tritanium 1000 pyerite 1000...\'');}
+		else if (args.length > 21) {return message.reply('Too many values :pensive: Keep it to 10 or less ')}
 		else {
 			await mongo().then(async function () {
 				function getBPrices(callback) {
@@ -44,9 +46,17 @@ module.exports = async (message, args) => {
 						for (let i = 0; i < args.length; i++) {
 							for (let j = 0; j < bPrices.length; j++) {
 								if (args[i].toLowerCase() === bPrices[j].item){
+									reportArray.push(args[i])
+									console.log(reportArray)
 									let buyPrice = parseFloat(bPrices[j].buy_price)
 									console.log(bPrices[j].buy_price)
+									reportArray.push(buyPrice)
+									console.log(reportArray)
+									reportArray.push(args[i+1])
+									console.log(reportArray)
 									quoteTotalThree.push(args[i + 1] * buyPrice);
+									reportArray.push(args[i + 1] * buyPrice)
+									console.log(reportArray);
 								}
 								console.log(quoteTotalThree);
 							}
@@ -65,6 +75,17 @@ module.exports = async (message, args) => {
 						let quoteOutputThree = quoteTotalThree.reduce((a, b) => a + b, 0);
 						quoteOutputThree = Math.round(quoteOutputThree);
 
+						let errorVal = (args.length / 2);
+						let reportVal = (reportArray.length / 4)
+						let errorName = "";
+
+						if (errorVal === reportVal){
+							errorName = "No"
+						}
+						else {
+							errorName = "Yes"
+						}
+
 
 						const disagree1 = '❌';
 						const agree1 = '☑️';
@@ -74,8 +95,17 @@ module.exports = async (message, args) => {
 							.setTitle('Status: Buy Mats Request')
 							.setAuthor(message.member.nickname, message.author.avatarURL())
 							.setColor(15105570)
-							.addFields({name: args, value: '----------', inline: true},
-								{name: 'Total isk', value: formatMoney(quoteOutputThree)}
+							.addFields({name: reportArray[2] + " " + reportArray[0] + " @ " + reportArray[1] , value: reportArray[3], inline: true},
+								{name: reportArray[6] + " " + reportArray[4] + " @ " + reportArray[5] , value: reportArray[7], inline: true},
+								{name: reportArray[10] + " " + reportArray[8] + " @ " + reportArray[9] , value: reportArray[11], inline: true},
+								{name: reportArray[14] + " " + reportArray[12] + " @ " + reportArray[13] , value: reportArray[15], inline: true},
+								{name: reportArray[18] + " " + reportArray[16] + " @ " + reportArray[17] , value: reportArray[19], inline: true},
+								{name: reportArray[22] + " " + reportArray[20] + " @ " + reportArray[21] , value: reportArray[23], inline: true},
+								{name: reportArray[26] + " " + reportArray[24] + " @ " + reportArray[25] , value: reportArray[27], inline: true},
+								{name: reportArray[30] + " " + reportArray[28] + " @ " + reportArray[29] , value: reportArray[31], inline: true},
+								{name: reportArray[34] + " " + reportArray[32] + " @ " + reportArray[33] , value: reportArray[35], inline: true},
+								{name: reportArray[38] + " " + reportArray[36] + " @ " + reportArray[37] , value: reportArray[39], inline: true},
+								{name: 'Total isk', value: formatMoney(quoteOutputThree)}, {name: "Errors?", value: errorName }
 							)
 							.setTimestamp()
 							.setFooter('Donate isk to corp and await mats')
@@ -136,5 +166,6 @@ module.exports = async (message, args) => {
 		}
 
 quoteTotalThree = [];
+		reportArray = [];
 }
 
